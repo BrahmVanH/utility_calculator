067c521c099b50dfaee36cb726ea215cf202cf10
Brahm Van Houzen <brahmvanh@gmail.com>
2023-11-17 22:49:47 -0500

    Added extra error handling to functions. continued writing tests

--- src/__tests__/calculations.test.ts
+++ src/__tests__/calculations.test.ts
@@ -1,10 +1,157 @@
-import { currentFromVoltRes, voltageFromCurRes, resistanceFromVoltCur, powerFromVoltCurr, poweFromCurrRes, powerFromVoltRes } from "../utils/calculations";
+import { currentFromVoltRes, voltageFromCurRes, resistanceFromVoltCur, powerFromVoltCurr, poweFromCurrRes, powerFromVoltRes } from '../utils/calculations';
 
 describe('calculates current/amperage from given voltage and resistance', () => {
-  // Test case 1
-  it('Should return a positive number given two positive numvers', () => {
-    const expectedResult = 25;
-    const result = currentFromVoltRes(5, 5);
-    expect(result).toBe(expectedResult)
-  })
-})
+	// Passing test
+	it('Should return a positive number given two positive numbers', () => {
+		const expectedResult = 1;
+		const result = currentFromVoltRes(5, 5);
+		expect(result).toBe(expectedResult);
+	});
+	// Exception case 1
+	it('Should throw an error if either variables is not a number', () => {
+		const voltage = 'string';
+		const resistance = 11;
+
+		const err = new Error('Voltage and resistance must be numbers');
+
+		const errCallback = () => {
+			currentFromVoltRes(voltage, resistance);
+		};
+
+		expect(errCallback).toThrowError(err);
+	});
+	// Exception case 2
+	it('Should throw an error if either variables is not a number', () => {
+		const voltage = 11;
+		const resistance = 'string';
+
+		const err = new Error('Voltage and resistance must be numbers');
+
+		const errCallback = () => {
+			currentFromVoltRes(voltage, resistance);
+		};
+
+		expect(errCallback).toThrowError(err);
+	});
+	// Exception case 3
+	it('Should throw an error if either variables is undefined', () => {
+		const voltage = undefined;
+		const resistance = 11;
+
+		const err = new Error('Voltage or resistance is undefined');
+
+		const errCallback = () => {
+			currentFromVoltRes(voltage, resistance);
+		};
+
+		expect(errCallback).toThrowError(err);
+	});
+	// Exception case 4
+	it('Should throw an error if either variables is undefined', () => {
+		const voltage = 11;
+		const resistance = undefined;
+
+		const err = new Error('Voltage or resistance is undefined');
+
+		const errCallback = () => {
+			currentFromVoltRes(voltage, resistance);
+		};
+
+		expect(errCallback).toThrowError(err);
+	});
+	//Exception case 5
+	it('Should throw an error if either number is negative', () => {
+		const voltage = -12;
+		const resistance = 12;
+
+		const err = new Error('Both parameters must be positive numbers');
+
+		const errCb = () => {
+			currentFromVoltRes(voltage, resistance);
+		};
+
+		expect(errCb).toThrowError(err);
+	});
+	//Exception case 6
+	it('Should throw an error if either number is negative', () => {
+		const voltage = 12;
+		const resistance = -12;
+
+		const err = new Error('Both parameters must be positive numbers');
+
+		const errCb = () => {
+			currentFromVoltRes(voltage, resistance);
+		};
+
+		expect(errCb).toThrowError(err);
+	});
+});
+
+describe('Given two number parameters, it should calculate voltage and return a representative number', () => {
+	// Passing test
+	it('Should return the product of two given numbers ', () => {
+		const expectedResult = 144;
+		const current = 12;
+		const resistance = 12;
+
+		const result = voltageFromCurRes(current, resistance);
+		expect(result).toBe(expectedResult);
+	});
+
+	// Exception test 1
+	it('Should throw an error if either parameter is undefined', () => {
+		const current = undefined;
+		const resistance = 100;
+		const err = new Error('Current or resistance is undefined');
+
+		const errCb = () => {
+			voltageFromCurRes(current, resistance);
+		};
+
+		expect(errCb).toThrowError(err);
+	});
+
+	// Exception test 2
+	it('Should throw an error if either parameter is undefined', () => {
+		const current = 100;
+		const resistance = undefined;
+		const err = new Error('Current or resistance is undefined');
+
+		const errCb = () => {
+			voltageFromCurRes(current, resistance);
+		};
+
+		expect(errCb).toThrowError(err);
+	});
+	// Exception test 3
+	it('Should throw an error if either parameter is not a number', () => {
+		const current = 12;
+		const resistance = 'string';
+		const err = new Error('Both parameters must be numbers');
+
+		const errCb = () => {
+			voltageFromCurRes(current, resistance);
+		};
+
+		expect(errCb).toThrowError(err);
+	});
+
+	// Exception test 4
+		it('Should throw an error if either parameter is not a number', () => {
+			const current = 'string';
+			const resistance = 12;
+			const err = new Error('Both parameters must be numbers');
+
+			const errCb = () => {
+				voltageFromCurRes(current, resistance);
+			};
+
+			expect(errCb).toThrowError(err);
+		});
+
+		// Exception test 5
+		it('Should throw an error if either number is negative', () => {
+			const current = -12;
+			cosnt resis
+		})
+});
--- src/utils/calculations.ts
+++ src/utils/calculations.ts
@@ -1,38 +1,72 @@
-// Volume
-// Area
-// Voltage
-// Wattage
-// Resistance
-// Amperage
-
-
 // Find current (amperage) from Voltage and Resistance
 export const currentFromVoltRes = (voltage: number, resistance: number): number => {
-  return voltage / resistance;
+  if (!voltage || !resistance) {
+    throw new Error("Voltage or resistance is undefined");
+  } else if (typeof voltage != 'number' || typeof resistance != 'number') {
+    throw new Error("Voltage and resistance must be numbers");
+  } else if (voltage < 0 || resistance < 0) {
+    throw new Error('Both parameters must be positive numbers');
+  }
+   return voltage / resistance;
 }
 
 // Find voltage using current and resistance 
 export const voltageFromCurRes = (current: number, resistance: number): number => {
+  if (!current || !resistance) {
+		throw new Error('Current or resistance is undefined');
+	} else if (typeof current != 'number' || typeof resistance != 'number') {
+		throw new Error('Both parameters must be numbers');
+	} else if (current < 0 || resistance < 0) {
+		throw new Error('Both parameters must be positive numbers');
+	}
   return current * resistance;
 }
 
 // Find resistance using voltage and current
 export const resistanceFromVoltCur = (voltage: number, current: number): number => {
+  if (!voltage || !current) {
+		throw new Error('One of the parameters is undefined');
+	} else if (typeof voltage != 'number' || typeof current != 'number') {
+		throw new Error('Both parameters must be numbers');
+	} else if (voltage < 0 || current < 0) {
+		throw new Error('Both parameters must be positive numbers');
+	}
   return voltage / current;
 }
 
 // Find power using voltage and current 
 export const powerFromVoltCurr = (voltage: number, current: number): number => {
+  if (!voltage || !current) {
+		throw new Error('One of the parameters is undefined');
+	} else if (typeof voltage != 'number' || typeof current != 'number') {
+		throw new Error('Both parameters must be numbers');
+	} else if (voltage < 0 || current < 0) {
+		throw new Error('Both parameters must be positive numbers');
+	}
   return voltage * current;
 };
 
 // Find power using current and resistance 
 export const poweFromCurrRes = (current: number, resistance: number): number => {
+   if (!current || !resistance) {
+			throw new Error('One of the parameters is undefined');
+		} else if (typeof current != 'number' || typeof resistance != 'number') {
+			throw new Error('Both parameters must be numbers');
+		} else if (current < 0 || resistance < 0) {
+			throw new Error('Both parameters must be positive numbers');
+		}
   return (Math.pow(current, 2) * resistance);
 }
 
 // Find power using voltage and resistance
 export const powerFromVoltRes = (voltage: number, resistance: number): number => {
+  if (!voltage || !resistance) {
+		throw new Error('One of the parameters is undefined');
+	} else if (typeof voltage != 'number' || typeof resistance != 'number') {
+		throw new Error('Both parameters must be numbers');
+	} else if (voltage < 0 || resistance < 0) {
+		throw new Error('Both parameters must be positive numbers');
+	}
   return (Math.pow(voltage, 2) / resistance);
 }
 
